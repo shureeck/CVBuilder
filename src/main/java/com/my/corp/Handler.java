9a27a9a440c3ccb18e055f9691e4e9c573d903ba
Oleksandr Poliakov <poliakovaleek@gmail.com>
2023-08-01 09:07:29 +0200

    Updated Handler.java

--- src/main/java/com/my/corp/Handler.java
+++ src/main/java/com/my/corp/Handler.java
@@ -5,11 +5,11 @@ import com.my.corp.utils.Utils;
 public class Handler {
     public Object handle(Object object) {
         class Resp {
-            final int statuSCode = 200;
-            final String message = "Lambda tests DONE";
+            public int statuSCode = 200;
+            public String message = "Lambda tests DONE";
         }
         System.out.println("Class: " + object.getClass());
-    //    System.out.println(Utils.objectToJSON(object));
+        System.out.println(Utils.objectToJSON(object));
 
         return Utils.objectToJSON(new Resp());
     }
