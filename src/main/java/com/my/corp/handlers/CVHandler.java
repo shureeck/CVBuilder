9544f2d2f6cb3225eb5a885a6c04ee27a314f270
Oleksandr Poliakov <poliakovaleek@gmail.com>
2023-08-03 00:09:32 +0200

    Added getting credentials from the SecretsManager

--- src/main/java/com/my/corp/handlers/CVHandler.java
+++ src/main/java/com/my/corp/handlers/CVHandler.java
@@ -10,10 +10,12 @@ import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 
+@Component
 public class CVHandler implements IHandler {
     @Autowired
     @Qualifier("CVDaoPostgreSQL")
