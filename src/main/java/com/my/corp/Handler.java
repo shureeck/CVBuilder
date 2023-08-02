fa8f48363686921301d9d09eb5931b2901fedf66
Oleksandr Poliakov <poliakovaleek@gmail.com>
2023-08-02 23:06:32 +0200

    Added getting credentials from the SecretsManager

--- src/main/java/com/my/corp/Handler.java
+++ src/main/java/com/my/corp/Handler.java
@@ -7,7 +7,9 @@ import com.my.corp.dao.Dao;
 import com.my.corp.utils.Utils;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.Objects;
 
 public class Handler {
     public APIGatewayProxyResponseEvent handle(LinkedHashMap<String, Object> object) {
@@ -16,14 +18,29 @@ public class Handler {
 
         if (object.get("path").equals("/cvs")
                 && object.get("httpMethod").equals("GET")) {
-            System.out.println("Path: /cvs; Method: GET");
-            Dao dao = new AnnotationConfigApplicationContext(SpringConfig.class)
-                    .getBean("CVDao", CVDao.class);
+            HashMap<String, String> parameters = (HashMap<String, String>) object.get("queryStringParameters");
+            if (Objects.isNull(parameters)) {
+                System.out.println("Path: /cvs; Method: GET");
+                Dao dao = new AnnotationConfigApplicationContext(SpringConfig.class)
+                        .getBean("CVDao", CVDao.class);
 
-            return new APIGatewayProxyResponseEvent()
-                    .withStatusCode(200)
-                    .withBody(Utils.objectToJSON(dao.get("poliakovaleek", null)))
-                    .withIsBase64Encoded(false);
+                return new APIGatewayProxyResponseEvent()
+                        .withStatusCode(200)
+                        .withBody(Utils.objectToJSON(dao.get("poliakovaleek", null)))
+                        .withIsBase64Encoded(false);
+            } else if (parameters.containsKey("cvID")) {
+                Dao dao = new AnnotationConfigApplicationContext(SpringConfig.class)
+                        .getBean("CVDaoPostgreSQL", CVDao.class);
+                return new APIGatewayProxyResponseEvent()
+                        .withStatusCode(200)
+                        .withBody(Utils.objectToJSON(dao.get("poliakovaleek", null)))
+                        .withIsBase64Encoded(false);
+            } else {
+                return new APIGatewayProxyResponseEvent()
+                        .withStatusCode(400)
+                        .withIsBase64Encoded(false)
+                        .withBody("Unknown set of parameters");
+            }
 
         }
 
