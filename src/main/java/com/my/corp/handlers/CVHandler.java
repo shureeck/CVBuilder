a64b3cacb2a9058ccb7d0e3da2380b4a1d1c10ee
Oleksandr Poliakov <poliakovaleek@gmail.com>
2023-08-09 00:15:28 +0200

    CORS fix

--- src/main/java/com/my/corp/handlers/CVHandler.java
+++ src/main/java/com/my/corp/handlers/CVHandler.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Component
 public class CVHandler implements IHandler {
@@ -36,6 +37,8 @@ public class CVHandler implements IHandler {
         }
         return new APIGatewayProxyResponseEvent()
                 .withStatusCode(200)
+                .withHeaders(Map.of("Access-Control-Allow-Origin", "*",
+                        "Content-Type", "application/json"))
                 .withBody(Utils.objectToJSON(entity))
                 .withIsBase64Encoded(false);
     }
